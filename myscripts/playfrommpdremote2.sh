2b75576040f1ba01985e9cf525a2e629fcb6c7e5
Koala887 <46446355+Koala887@users.noreply.github.com>
2025-01-02 15:12:24 +0100

    Update playfrommpdremote2.sh

--- myscripts/playfrommpdremote2.sh
+++ myscripts/playfrommpdremote2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #########################################################################
-##  frankl (C) 2015              playfrommpd
+##  frankl (C) 2015              playfrommpdremote
 ##
 ##  See README or http://frank_l.bitbucket.org/stereoutils/player.html
 ##  for explanations.
@@ -17,50 +17,40 @@ PORT=5570
 #VERBOSE="--verbose --verbose"
 SAMPLERATE=$1
 BITLENGTH=$2
+BITLENGTH=32
+SAMPLEFORMAT=S32_LE
 
 FILENAME=`echo "$3" | sed -e "s\.*musik\/mnt/media/musik\g;s/*20/ /g"` 
 
 echo $3
 echo "File name is ${FILENAME}"
 
-BITLENGTH=32
-SAMPLEFORMAT=S32_LE
 if [ "${SAMPLERATE}" = "96000" ]; then
-  FILESIZE=5120
   LOOPSPERSECOND=1500
-  BLOCKSIZE=512
   HW_BUFFER=4096
   EXTRABYTESPLAY=0
   BYTESPERSECOND=768000
 
 elif [ "${SAMPLERATE}" = "48000" ]; then
-  FILESIZE=2560
   LOOPSPERSECOND=1500
-  BLOCKSIZE=256
   HW_BUFFER=2048
   EXTRABYTESPLAY=0
   BYTESPERSECOND=384000
 
 elif [ "${SAMPLERATE}" = "88200" ]; then
-  FILESIZE=4800 #6720
   LOOPSPERSECOND=1050
-  BLOCKSIZE=480 #672
   HW_BUFFER=4032
   EXTRABYTESPLAY=0
   BYTESPERSECOND=705600
 
 elif [ "${SAMPLERATE}" = "44100" ]; then
-  FILESIZE=3360
   LOOPSPERSECOND=1050
-  BLOCKSIZE=336
   HW_BUFFER=7680
   EXTRABYTESPLAY=0
   BYTESPERSECOND=352800
 
 elif [ "${SAMPLERATE}" = "192000" ]; then
-  FILESIZE=10240
   LOOPSPERSECOND=1500
-  BLOCKSIZE=1024
   HW_BUFFER=8192
   EXTRABYTESPLAY=0
   BYTESPERSECOND=1536000
@@ -68,9 +58,9 @@ fi
 
 trap "trap - SIGTERM && kill -- -$$ " SIGINT SIGTERM EXIT
 
-cpupower frequency-set -u 3400MHz
+cpupower frequency-set -u 3400MHz >/dev/null 2>&1
 sox "$3" -t raw -e signed -b 32 /dev/shm/play.raw
-cpupower frequency-set -u 800MHz -d 800MHz
+cpupower frequency-set -u 800MHz -d 800MHz >/dev/null 2>&1
 
 # tell audio computer what to do, short sleep as startup time for sender
 REMOTE="sleep 0.3;\
@@ -84,7 +74,7 @@ REMOTE="sleep 0.3;\
 
 echo "${REMOTE}" |  nc -q 0 ${AUDIOCOMPUTER} 5501 
 
-taskset -c 1 chrt -f 95 bufhrtmin --file=/dev/shm/play.raw --bytes-per-second=${BYTESPERSECOND} \
+taskset -c 1 chrt -f 90 bufhrtmin --file=/dev/shm/play.raw --bytes-per-second=${BYTESPERSECOND} \
        --loops-per-second=${LOOPSPERSECOND} --port-to-write=${PORT} \
        --extra-bytes-per-second=${EXTRABYTESBUF} \
        ${VERBOSE} #--out-net-buffer-size=67200
