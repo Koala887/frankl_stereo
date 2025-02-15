2f8971b6ac06076d15b78a814b26021ad7708cc6
Koala887 <46446355+Koala887@users.noreply.github.com>
2025-02-15 20:35:08 +0100

    Update highrestest.c

--- src/highrestest.c
+++ src/highrestest.c
@@ -91,7 +91,7 @@ int main(int argc, char *argv[]) {
         res.tv_sec++;
         res.tv_nsec -= 1000000000;
       }
-      clock_nanosleep(MYCLOCK, TIMER_ABSTIME, &res, NULL);
+      while (clock_nanosleep(MYCLOCK, TIMER_ABSTIME, &res, NULL) != 0);
       clock_gettime(MYCLOCK, &tim);
       d = difftimens(last, tim)-step;
       k = d/dint;
